fd2b36d41331a27fb03595d549ea380951fb4202
Chris Wright <chris@inkyspider.co.uk>
2021-06-18 00:24:47 +0100

    Modify Frame API to allow external control

--- frame.js
+++ frame.js
@@ -3,37 +3,32 @@ function random_hsl() {
 }
 
 class Frame {
-    aspect_ratio = 4/3
-    fade_in_time = 5
-    fade_out_time = 10
-
-    constructor(scale) {
+    constructor(position, scale, aspect_ratio) {
+        this.position = position
         this.scale = scale
-
-        this.x_rand = Math.random()
-        this.y_rand = Math.random()
+        this.aspect_ratio = aspect_ratio
 
         this.container = document.createElement("div")
         this.container.classList.add("frame")
         this.container.style.position = "absolute"
         this.container.style.backgroundColor = random_hsl()
-        this.container.style.opacity = 0
+        this.container.style.opacity = "0"
         this.update()
     }
 
     update() {
-        var pixel_height = window.innerHeight * this.scale / 100
-        var pixel_width = pixel_height * this.aspect_ratio
+        this.pixel_height = window.innerHeight * this.scale
+        this.pixel_width = this.pixel_height * this.aspect_ratio
 
-        if (100 * pixel_width / window.innerWidth > this.scale) {
-            pixel_width = window.innerWidth * this.scale / 100
-            pixel_height = pixel_width / this.aspect_ratio
+        if (this.pixel_width / window.innerWidth > this.scale) {
+            this.pixel_width = window.innerWidth * this.scale
+            this.pixel_height = this.pixel_width / this.aspect_ratio
         }
 
-        this.container.style.left = `${this.x_rand * (window.innerWidth - pixel_width)}px`
-        this.container.style.top = `${this.y_rand * (window.innerHeight - pixel_height)}px`
-        this.container.style.width = `${pixel_width}px`
-        this.container.style.height = `${pixel_height}px`
+        this.container.style.left = `${this.position.x * (window.innerWidth - this.pixel_width)}px`
+        this.container.style.top = `${this.position.y * (window.innerHeight - this.pixel_height)}px`
+        this.container.style.width = `${this.pixel_width}px`
+        this.container.style.height = `${this.pixel_height}px`
     }
 
     populate_with_image(src) {
@@ -42,16 +37,16 @@ class Frame {
         this.container.appendChild(image)
     }
 
-    fade_in() {
+    fade_in(duration) {
         this.container.style.animationName = "fade_in"
-        this.container.style.animationDuration = `${this.fade_in_time}s`
+        this.container.style.animationDuration = `${duration}s`
         this.container.style.animationFillMode = "forwards"
         this.container.style.animationDelay = "0.5s"
     }
 
-    fade_out() {
+    fade_out(duration) {
         this.container.style.animationName = "fade_out"
-        this.container.style.animationDuration = `${this.fade_out_time}s`
+        this.container.style.animationDuration = `${duration}s`
         this.container.style.animationFillMode = "forwards"
         this.container.style.animationDelay = "0s"
     }
