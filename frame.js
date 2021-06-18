fe33b964eb59ee8abb379ebdf0aab2a0c7ff9449
Chris Wright <chris@inkyspider.co.uk>
2021-06-18 02:07:10 +0100

    Manage frames with Collage object

--- frame.js
+++ frame.js
@@ -2,6 +2,38 @@ function random_hsl() {
     return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
 }
 
+class Collage {
+    constructor() {
+        this.aspect_ratio = 4/3
+        this.scale = 0.37
+
+        this.fade_in_time = 5
+        this.fade_out_delay = 10
+        this.fade_out_time = 10
+
+        this.active_frames = []
+    }
+
+    update() {
+        this.active_frames.forEach(frame => frame.update())
+    }
+
+    insert_new_frame() {
+        let frame = new Frame({x: Math.random(), y: Math.random()}, this.scale, this.aspect_ratio)
+        this.active_frames.push(frame)
+        frame.add_to_body()
+        frame.fade_in(this.fade_in_time)
+        let instance = this
+        setTimeout(function() {
+            frame.fade_out(instance.fade_out_time)
+        }, instance.fade_out_delay * 1000)
+        setTimeout(function() {
+            frame.remove_from_body()
+            instance.active_frames = instance.active_frames.filter(element => element !== frame)
+        }, instance.fade_out_delay * 1000 + (instance.fade_out_time * 1000))
+    }
+}
+
 class Frame {
     constructor(position, scale, aspect_ratio) {
         this.position = position
@@ -25,10 +57,32 @@ class Frame {
             this.pixel_height = this.pixel_width / this.aspect_ratio
         }
 
-        this.container.style.left = `${this.position.x * (window.innerWidth - this.pixel_width)}px`
-        this.container.style.top = `${this.position.y * (window.innerHeight - this.pixel_height)}px`
+        this.pixel_left = this.position.x * (window.innerWidth - this.pixel_width)
+        this.pixel_top = this.position.y * (window.innerHeight - this.pixel_height)
+
+        this.container.style.left = `${this.pixel_left}px`
+        this.container.style.top = `${this.pixel_top}px`
         this.container.style.width = `${this.pixel_width}px`
         this.container.style.height = `${this.pixel_height}px`
+    }
+
+    contains_coordinate(coordinate) {
+        let within_horizontal = coordinate.x >= this.pixel_left && coordinate.x <= this.pixel_left + this.pixel_width
+        let within_vertical = coordinate.y >= this.pixel_top && coordinate.y <= this.pixel_top + this.pixel_height
+        return within_horizontal && within_vertical
+    }
+
+    get corners() {
+        return [
+            {x: this.pixel_left, y: this.pixel_top},
+            {x: this.pixel_left + this.pixel_width, y: this.pixel_top},
+            {x: this.pixel_left, y: this.pixel_top + this.pixel_height},
+            {x: this.pixel_left + this.pixel_width, y: this.pixel_top + this.pixel_height},
+        ]
+    }
+
+    overlaps(other_frame) {
+        return this.corners.some(corner => other_frame.contains_coordinate(corner))
     }
 
     populate_with_image(src) {
